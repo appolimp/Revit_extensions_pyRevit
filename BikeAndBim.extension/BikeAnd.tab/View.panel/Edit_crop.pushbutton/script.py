71a0bc0db04bb6ebb9f59a1ce56037a5864ad2e3
appolimp <65594849+appolimp@users.noreply.github.com>
2020-12-16 18:46:24 +0300

    Update Edit_crop
    Add work to non ortogonal plane and view

--- BikeAndBim.extension/BikeAnd.tab/View.panel/Edit_crop.pushbutton/script.py
+++ BikeAndBim.extension/BikeAnd.tab/View.panel/Edit_crop.pushbutton/script.py
@@ -1,114 +1,213 @@
 # coding=utf-8
-import os.path
-import sys
+from rpw import db, DB, UI, uidoc, doc, logger
 
 
-panel_path = os.path.split(sys.path[0])[0]
-tab_path = os.path.split(panel_path)[0]
-ext_path = os.path.split(tab_path)[0]
-base_path = os.path.join(ext_path, 'base')
-sys.path.append(base_path)
+def task_dialog(msg):
+    """
+    For create task dialog with error and message
 
+    :param msg: Message for window
+    :type msg: str
+    """
 
-from wrapper import DB, doc, uidoc, transaction, ScriptError
-from Autodesk.Revit.UI.Selection import PickBoxStyle
+    window = UI.TaskDialog('Edit crop')
+    window.TitleAutoPrefix = False
 
-import logging
+    window.MainIcon = UI.TaskDialogIcon.TaskDialogIconError
+    window.MainInstruction = 'Error'
+    window.MainContent = msg
 
-
-@transaction(msg='Edit crop view')
-def main():
-    edit_view_crop()
-
-
-def edit_view_crop():
-
-    box = DB.BoundingBoxXYZ()
-    user_select_rect = box_selection()
-    box.Min, box.Max = convert_two_point(*user_select_rect)
-
-    doc.ActiveView.CropBox = box
-    return box
+    window.CommonButtons = UI.TaskDialogCommonButtons.Ok
+    window.Show()
 
 
 def box_selection():
     """
-        Get two points by user rectangle selection
+    Get two points by user rectangle selection
 
-        :returns (DB.XYZ, DB.XYZ)
-        """
-    data = uidoc.Selection.PickBox(PickBoxStyle.Enclosing, 'select two point')
+    :returns (DB.XYZ, DB.XYZ)
+    :rtype tuple
+    """
+
+    style = UI.Selection.PickBoxStyle.Enclosing
+    data = uidoc.Selection.PickBox(style, 'select two point')
     return data.Min, data.Max
 
 
-def transform_point_for_section(first, second, side, flip_direction):
+@db.Transaction.ensure('Edit crop view')
+def set_view_crop_by_point(view, down_left, up_right):
     """
-    Convert point for section
-    Using origin shaft and view direction
+    Set view crop of view by min and max point
 
+    :param view: DB.View
+    :param down_left: DB.XYZ
+    :param up_right: DB.XYZ
+    """
+
+    box = view.CropBox
+
+    box.Min = down_left + box.Min.Z * DB.XYZ.BasisZ
+    box.Max = up_right + box.Max.Z * DB.XYZ.BasisZ
+
+    doc.ActiveView.CropBox = box
+
+
+def calc_x_y_plan(view, point):
+    """
+    Calculate point coordinate in plane of plane view
+
+    - Calculate offset of view origin
+    - Rotate point to angle between view.UpDirection and DB.XYZ.BasisY
+
+    :param view: DB.ViewPlan
+    :param point: DB.XYZ
+    :return: DB.XYZ in plane view
+    """
+
+    normal_val = point - view.Origin - (point - view.Origin).Z * DB.XYZ.BasisZ
+
+    angle = calc_angle(view.UpDirection, DB.XYZ.BasisY)
+    rot_point = rotate_by_point_and_angle(normal_val, DB.XYZ.Zero, angle)
+
+    return rot_point
+
+
+def calc_angle(main_vector, second_vector):
+    """
+    Calculate angle between two vectors
+
+    :param main_vector: DB.XYZ
+    :param second_vector: DB.XYZ
+    :return: Angle with sign
+    :rtype: float
+    """
+    angle = main_vector.AngleTo(second_vector)
+    sign = 1 if main_vector.CrossProduct(second_vector).Z >= 0 else -1
+    return sign * angle
+
+
+def rotate_by_point_and_angle(vector, origin, angle):
+    """
+    Rotate vector at origin to angle
+
+    :param vector: DB.XYZ
+    :param origin: DB.XYZ of origin
+    :param angle: Angle to rotate
+    :type angle: float
+    :return: DB.XYZ
+    """
+
+    transform = DB.Transform.CreateRotationAtPoint(DB.XYZ.BasisZ, angle, origin)
+    rot_vector = transform.OfPoint(vector)
+
+    return rot_vector
+
+
+def calc_x_y_section(view, point):
+    """
+    Calculate point coordinate in plane of section view
+
+    - Calculate offset of view origin
+    - Get shift in plane of view
+    - Transform Z value to Y
+
+    :param view: DB.ViewSection
+    :param point: DB.XYZ
+    :return: DB.XYZ in plane view
+    """
+
+    height = (point - view.Origin).Z
+    normal_val = point - view.Origin - height * DB.XYZ.BasisZ
+
+    length = normal_val.GetLength()
+    sign = 1 if view.ViewDirection.CrossProduct(normal_val).Z >= 0 else -1
+
+    new_point = DB.XYZ(sign * length, height, 0)
+
+    return new_point
+
+
+def calc_up_down(p_first, p_second):
+    """
+    Convert point for up_right and down_left
+
+    :param p_first: DB.XYZ
+    :param p_second: DB.XYZ
+    :return: (DB.XYZ, DB.XYZ)
+    :rtype: tuple
+    """
+
+    p_left = DB.XYZ(min(p_first.X, p_second.X),
+                    min(p_first.Y, p_second.Y),
+                    0)
+
+    p_right = DB.XYZ(max(p_first.X, p_second.X),
+                     max(p_first.Y, p_second.Y),
+                     0)
+
+    return p_left, p_right
+
+
+def calc_point_by_handler(view, first, second, handler):
+    """
+    Calculate points for box by handler
+
+    - Calculate in plane
+    - Modify to up_right and down_left
+
+    :param view: DB.View
     :param first: DB.XYZ
     :param second: DB.XYZ
-    :param side: str
-    :param flip_direction: bool
-
-    :returns (DB.XYZ, DB.XYZ)
+    :param handler: Func for transform point for view
+    :type handler: lambda view, point: pass
+    :return: (DB.XYZ, DB.XYZ)
+    :rtype: tuple
     """
-    origin = doc.ActiveView.Origin
 
-    point_x = [getattr(elem, side) for elem in (first, second)]
-    point_z = [getattr(elem, 'Z') for elem in (first, second)]
+    p_first, p_second = handler(view, first), handler(view, second)
 
-    point_left = DB.XYZ(min(point_x) - getattr(origin, side), min(point_z) - origin.Z, 0)
-    point_right = DB.XYZ(max(point_x) - getattr(origin, side), max(point_z) - origin.Z, 0)
+    p_min, p_max = calc_up_down(p_first, p_second)
 
-    if flip_direction:  # Need for flip section
-        left_x, right_x = point_left.X, point_right.X
-        point_left = DB.XYZ(-right_x, point_left.Y, 0)
-        point_right = DB.XYZ(-left_x, point_right.Y, 0)
-
-    return point_left, point_right
+    return p_min, p_max
 
 
-def convert_two_point(first, second):
+def edit_crop(view):
     """
-        Convert point for BoundingBoxXYZ
-        return left_down and right_up points
+    Edit crop view
 
-        :param first: DB.XYZ
-        :param second: DB.XYZ
+    :param view:
+    :type view:
+    :return:
+    :rtype:
+    """
 
-        :returns (DB.XYZ, DB.XYZ)
-        """
-    direction = doc.ActiveView.ViewDirection
-
-    if round(direction.X - direction.Y, 5) == 0:  # View plane
-        point_left = DB.XYZ(min(first.X, second.X), min(first.Y, second.Y), 0)
-        point_right = DB.XYZ(max(first.X, second.X), max(first.Y, second.Y), 0)
-
-    elif round(direction.Y - direction.Z, 5) == 0:  # Section in X
-        point_left, point_right = transform_point_for_section(
-            first, second, side='Y',
-            flip_direction=int(direction.X) == -1)
-
-    elif round(direction.X - direction.Z, 5) == 0:  # Section in Y
-        point_left, point_right = transform_point_for_section(
-            first, second, side='X',
-            flip_direction=int(direction.Y) == 1)
-
+    if view.ViewType in VALID_VIEW_TYPE:
+        handler = VALID_VIEW_TYPE[view.ViewType]
     else:
-        raise NotImplemented("3D and slope section doesn't work")  # FIXME
-    return point_left, point_right
+        raise NotImplementedError('View type "{}" not designed'.format(doc.ActiveView.ViewType))
+
+    first, second = box_selection()
+    p_min, p_max = calc_point_by_handler(view, first, second, handler)
+    set_view_crop_by_point(view, p_min, p_max)
+
+
+VALID_VIEW_TYPE = {
+    DB.ViewType.Section: calc_x_y_section,
+    DB.ViewType.Elevation: calc_x_y_section,
+    DB.ViewType.EngineeringPlan: calc_x_y_plan,
+    DB.ViewType.FloorPlan: calc_x_y_plan,
+}
+
+
+def main():
+    view = doc.ActiveView
+    edit_crop(view)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        filename=None, level=logging.INFO,
-        format='[%(asctime)s] %(levelname).1s <example>: %(message)s',
-        datefmt='%Y.%m.%d %H:%M:%S')
+    logger.setLevel(50)
 
     try:
-        OUT = main()
-    except ScriptError as e:
-        logging.error(e)
+        main()
     except Exception as err:
-        logging.exception('Critical error')
+        task_dialog(msg=str(err.args[0]) + '\nPlease, write Nikita')
