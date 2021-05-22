a0df4cbf25695d464ab41a42d626b4adf575e683
appolimp <appolimp@mail.ru>
2021-05-22 17:10:33 +0300

    to print

--- BikeAndBim.extension/BikeAnd.tab/Dimension.panel/test_dim.pushbutton/script.py
+++ BikeAndBim.extension/BikeAnd.tab/Dimension.panel/test_dim.pushbutton/script.py
@@ -1,9 +1,16 @@
 # coding=utf-8
-from dim_column import create_dim_for_column, ScriptError
+# Функция для тестирования
+from dim_column import create_dim_for_column, is_need_shift
 
-import unittest
-from pyrevit.unittests.runner import OutputWriter, PyRevitTestRunner, RESULT_TEST_SUITE_START
-from rpw import revit, db, ui, doc, uidoc, logger, DB, UI
+import unittest  # стандартный модуль библиотеки Python
+# модули расширения PyRevit
+from pyrevit.unittests.runner import (OutputWriter, PyRevitTestRunner,
+                                      RESULT_TEST_SUITE_START)
+from rpw import doc, logger, DB, db, ScriptError
+
+# Собственные функции для создания объектов. В данной работе не приводятся
+from creator import (create_axis, create_column,
+                     get_view_project_browser, create_wall)
 
 
 def run_suite_tests(test_suite, name):
@@ -16,10 +23,8 @@ class TestDimColumn(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.column = create_column(location=(0, 0 , 0), height=3000)
-        cls.column = doc.GetElement(DB.ElementId(553068))
+        cls.column = create_column(location=(0, 0, 0), height=3000)
         cls.axis = create_axis(origin=(0, 0, 0), direction=(0, 1, 0))
-        cls.axis = doc.GetElement(DB.ElementId(518439))
         cls.view = doc.ActiveView
 
     @classmethod
@@ -40,26 +45,34 @@ class TestDimColumn(unittest.TestCase):
 
     def test_02_dim_on_view(self):
         dim = create_dim_for_column(self.column, view=self.view)
-        self.assertEqual(dim.View.Id, self.view.Id, 'Размеры созданы не на заданном виде')
+        self.assertEqual(dim.View.Id, self.view.Id, 'Ошибка с видом')
 
     def test_03_count_segments(self):
         dim = create_dim_for_column(self.column, view=self.view)
-        self.assertEqual(dim.Segments.Size, 2, 'Размеры состоят не из двух сегментов')
+        self.assertEqual(dim.Segments.Size, 2, 'Ошибка с сегментами')
 
     def test_04_correct_column(self):
         dim = create_dim_for_column(self.column, view=self.view)
-        self.assertIn(self.column.Id, [ref.ElementId for ref in dim.References], 'Размер не относится к колонне')
+        depended_ids = [ref.ElementId for ref in dim.References]
+        self.assertIn(self.column.Id, depended_ids, 'Ошибка с колонной')
 
     def test_05_correct_axis(self):
         dim = create_dim_for_column(self.column, view=self.view)
-        self.assertIn(self.axis.Id, [ref.ElementId for ref in dim.References], 'Размер не относится к оси')
+        depended_ids = [ref.ElementId for ref in dim.References]
+        self.assertIn(self.axis.Id, depended_ids, 'Ошибка с осью')
 
     def test_06_check_is_shift_segments(self):
+        self.assertTrue(False)
         dim = create_dim_for_column(self.column, view=self.view)
 
         for segment in dim.Segments:
-            if is_need_shift(value=segment.ValueString, dim_type=dim.DimensionType, scale=self.view.Scale):
-                self.assertGreater(segment.Origin.DistanceTo(segment.TextPosition), 0.5, 'Сегмент размера не смещен')
+            value = segment.ValueString
+            dim_type = dim.DimensionType
+            scale = self.view.Scale
+
+            if is_need_shift(value, dim_type, scale):
+                shift = segment.Origin.DistanceTo(segment.TextPosition)
+                self.assertGreater(shift, 0.5, 'Сегмент не смещен')
 
     def test_07_error_with_not_column(self):
         wall = create_wall(origin=(0, 0, 0), direction=(1, 0, 0), level=0)
@@ -72,30 +85,12 @@ class TestDimColumn(unittest.TestCase):
             create_dim_for_column(self.column, view=non_valid_view)
 
 
-def create_axis(**kwargs):
-    return []
-
-
-def create_column(**kwargs):
-    return []
-
-
-def get_view_project_browser():
-    return []
-
-
-def create_wall(**kwargs):
-    return []
-
-
-def is_need_shift(*args, **kwargs):
-    return True
-
-
 def main():
-    logger.title('Start testing')
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestDimColumn)
-    run_suite_tests(suite, name=TestDimColumn.__name__)
+    # Общая транзакция для создания элементов
+    with db.TransactionGroup(name='Test Column'):
+        logger.title('Start testing')
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestDimColumn)
+        run_suite_tests(suite, name=TestDimColumn.__name__)
 
 
 if __name__ == '__main__':
